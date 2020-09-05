00aafaafbf7352407b348b4b255af0323370697d
subr1990 <subramaniank@live.com>
2020-09-05 20:30:12 +1000

    dsf

--- src/main/java/testboot/TestbootApplication.java
+++ src/main/java/testboot/TestbootApplication.java
@@ -3,11 +3,15 @@ package testboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+@RequestMapping("/hello")
 public class TestbootApplication {
 
-    @GetMapping("/")
+    @GetMapping("/mt")
     public String greeting(){
         return "greeting";
     }
