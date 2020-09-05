bbc0948bf41b1d0fea4ffdf1d9e165958dda858e
subr1990 <subramaniank@live.com>
2020-09-05 22:48:30 +1000

    Update TestbootApplication.java

--- src/main/java/testboot/TestbootApplication.java
+++ src/main/java/testboot/TestbootApplication.java
@@ -2,9 +2,12 @@ package testboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @SpringBootApplication
@@ -16,6 +19,16 @@ public class TestbootApplication {
         return "greeting";
     }
 
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*");
+            }
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(TestbootApplication.class, args);
     }
